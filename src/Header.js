f6987588c4b698d375552e44b04398a84769e41e
Sari Stissi <sestissi16@earlham.edu>
2017-06-13 19:45:15 -0400

    Make logo into a separate React component nested inside the header component

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import './Header.css'
 import NavLinks from './NavLinks'
+import Logo from './Logo'
 
 class Header extends Component{
     render(){
         return (
         <div className="expanded row header">
           <div className="clearfix">
-            <div className="float-left logo">
-              <img src="http://www.iconninja.com/files/409/4/1006/axe-icon.svg" alt="axe" />
-            </div>
+            <Logo />
             <NavLinks />
           </div>
           <div className="headline text-center small-12 columns">
