383e0ce3721ace70644a8f5fa41a468ef2d1fdbf
Sari Stissi <sestissi16@earlham.edu>
2017-06-13 20:48:12 -0400

    Make article start a separate React component nested in Article body

---
+++ src/ArticleStart.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './ArticleStart.css'
+
+class ArticleStart extends Component{
+    render(){
+        return(
+            <span className="article-start gold">E</span>
+        )
+    }
+}
+
+export default ArticleStart
