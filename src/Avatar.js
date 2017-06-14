9afd56e7017c0d815f4637a1bcfdee6502466ce4
Sari Stissi <sestissi16@earlham.edu>
2017-06-13 20:27:55 -0400

    Make avatar image a separate React component nested in Avatar

--- src/Avatar.js
+++ src/Avatar.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './Avatar.css'
+import AvatarImage from './AvatarImage'
 
 class Avatar extends Component{
     render(){
         return(
             <div className="avatar">
-                <img src="http://www.zbrushcentral.com/attachment.php?attachmentid=376082" alt="author" />
+                <AvatarImage />
                 <div className="author-info">
                     <p className="author-name">By Domri, son of Flug</p>
                     <p className="date">on 28 April, 3018 of the Third Age</p>
---
+++ src/AvatarImage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './AvatarImage.css'
+
+class AvatarImage extends Component{
+    render(){
+        return(
+            <img src="http://www.zbrushcentral.com/attachment.php?attachmentid=376082" alt="author" />
+        )
+    }
+}   
+
+export default AvatarImage
