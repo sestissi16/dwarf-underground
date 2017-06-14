ff435e53d440890b8d31193088fab3ec0b28be3c
Sari Stissi <sestissi16@earlham.edu>
2017-06-13 20:17:32 -0400

    Make the avatar as a separate React Component nested in the Article

---
+++ src/ArticleTitle.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './ArticleTitle.css'
+
+class ArticleTitle extends Component{
+    render(){
+        return(
+            <h2 className="article-title">Gold Madness - Fact or Fiction?</h2>
+        )
+    }
+}
+
+export default ArticleTitle
